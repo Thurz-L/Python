bd9c7884d8c0390dfabb3d5b2c70e313f423357c
Arthur Lopes <108228697+Thurz-L@users.noreply.github.com>
2022-09-14 12:16:48 -0300

    Add files via upload

---
+++ Metodo Format.py
@@ -0,0 +1,13 @@
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+
+print('O faturamento da loja foi ' + str(faturamento) + '.O custo da loja foi ' + str(custo) + '.Assim, o lucro da loja foi de ' + str(lucro))
+
+### Método format
+
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+
+print("O faturamento da loja foi {}. O custo da loja foi {}. Assim, o lucro da loja foi de {}".format(faturamento,custo,lucro))
